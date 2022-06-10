2bdfd221a94d1f4cd447ab2680316c5adc47417e
Avensen Nejersian <anejersian@gmail.com>
2022-06-10 19:05:57 -0400

    Update  blogCardsInfo schema

--- src/lib/pseudo/blog.js
+++ src/lib/pseudo/blog.js
@@ -1,14 +1,23 @@
 export const blogCardsInfo = [
 	{
 		text: '<b class="font-semibold">¿Miedo al fracaso?</b><br />Tips para enfrentarlo',
-		image: 'dimg_1.png'
+		image: {
+			src: 'dimg_1.png',
+			alt: ''
+		}
 	},
 	{
 		text: '<b class="font-semibold">5 consejos</b> Para dar un servicio de excelencia',
-		image: 'dimg_2.png'
+		image: {
+			src: 'dimg_2.png',
+			alt: ''
+		}
 	},
 	{
 		text: '<b class="font-semibold">En mi negocio no quiero fallos</b> ¿Error o logro?',
-		image: 'dimg_3.png'
+		image: {
+			src: 'dimg_3.png',
+			alt: ''
+		}
 	}
 ];
