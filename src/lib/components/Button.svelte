5154b9aed76a11a7864aa434b5fc70000c1de59e
Avensen Nejersian <anejersian@gmail.com>
2022-07-17 01:11:18 -0400

    Add vite config file

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -14,25 +14,13 @@
 
 <a {href} {target} class="{variants.base} {variants[variant]} effect">{text}</a>
 
-<!-- <button type="button" class="base uppercase inline-flex items-center px-8 py-2 border border-transparent text-sm font-medium rounded-lg shadow-sm text-white bg-primary hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{text}</button> -->
 <style lang="postcss">
 	.effect {
-		@apply transition ease-out hover:scale-105 duration-150;
+		@apply transition ease-out hover:scale-105 duration-75;
 	}
 
 	.base {
 		@apply uppercase py-2 px-7 rounded-xl text-sm inline-block font-medium;
 	}
 
-	.btn {
-		@apply inline-flex items-center
-			px-8 py-2
-			border border-transparent
-			text-sm font-medium
-			rounded-md
-			shadow-sm
-			text-white bg-indigo-600
-			hover:bg-indigo-700
-			focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500;
-	}
 </style>
